f0407793977655bb7c44cb9d0c43e58299b8d9ad
juliocesardw <67384316+juliocesardw@users.noreply.github.com>
2021-09-28 13:23:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+const itemProduct=document.querySelectorAll('.grid-item');
+
+itemProduct.forEach(item => {
+    const imgs=item.querySelectorAll('.data-images img');
+
+    const colors=item.querySelectorAll('.data-colors a');
+
+    colors.forEach(color => {
+        color.addEventListener('click',(e)=>{
+            e.preventDefault();
+
+            colors.forEach(col => {
+                col.innerHTML="";
+            });
+
+            const idColor=color.getAttribute('data-id');
+
+            const i =document.createElement('i');
+            i.setAttribute('class',"fas fa-check");
+
+            imgs.forEach(img => {
+                img.classList.remove('img-active');
+
+                const idImage=img.getAttribute('data-id');
+
+                if(idColor==idImage){
+                    img.classList.add('img-active');
+                    color.appendChild(i);
+                }
+            });
+
+        })
+    });
+
+
+});
